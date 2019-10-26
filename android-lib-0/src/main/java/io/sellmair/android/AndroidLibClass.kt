cec8d33457562be1b6d1c1c4ea86ba2c99b5892e
sellmair <sebastiansellmair@gmail.com>
2019-10-26 13:24:32 +0200

    Add demonstrstion for bug

--- android-lib-0/src/main/java/io/sellmair/android/AndroidLibClass.kt
+++ android-lib-0/src/main/java/io/sellmair/android/AndroidLibClass.kt
@@ -4,6 +4,13 @@ import Lib1JvmMainClass
 
 class AndroidLibClass {
     fun main() {
+
+        /**
+         * This expression will break in the ide after  ./gradlew jvmJar  is executed with
+         * "Check you module classpath for missing or conflicting dependencies"
+         *
+         * The same expression will be fine after  ./gradlew clean  is executed
+         */
         Lib1JvmMainClass().lib0CommonMainClass.sayHello()
     }
 }
