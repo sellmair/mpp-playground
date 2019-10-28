20738ab8096778d4e4953a1e06f762af481d82f3
sellmair <sebastiansellmair@gmail.com>
2019-10-28 08:22:43 +0100

    Add issue workaround

--- android-lib-0/build.gradle.kts
+++ android-lib-0/build.gradle.kts
@@ -1,13 +1,18 @@
 plugins {
     id("com.android.library")
-    id("kotlin-android")
+    id("kotlin-multiplatform")
 }
 
 android {
     compileSdkVersion(28)
 }
 
-dependencies {
+kotlin {
+    android()
+}
+
+kotlin.sourceSets.getByName("androidMain").dependencies {
     implementation(project(":mpp-lib-1"))
+    implementation(project(":mpp-lib-0"))
     implementation(kotlin("stdlib-jdk8"))
 }
