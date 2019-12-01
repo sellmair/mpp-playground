60564d4772e61f88f3888d09e9f8aba961e1a5e3
sellmair <sebastiansellmair@gmail.com>
2019-12-01 01:11:58 +0100

    Add single platform app

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,8 @@
 buildscript {
+    allprojects {
+        extra["kotlin_version"] = "1.3.70-SELLMAIR"
+    }
+
     repositories {
         maven { setUrl("https://dl.bintray.com/kotlin/kotlin-eap") }
         maven { setUrl("https://dl.bintray.com/kotlin/kotlin-dev") }
@@ -9,7 +13,7 @@ buildscript {
     }
 
     dependencies {
-        classpath(kotlin("gradle-plugin:1.3.70-SELLMAIR"))
+        classpath(kotlin("gradle-plugin:${rootProject.extra["kotlin_version"]}"))
         classpath("com.android.tools.build:gradle:3.5.2")
 
     }
